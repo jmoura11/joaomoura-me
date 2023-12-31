1b41dc75cfba458535c7a04be350a4faab2050d3
jmoura11 <joaosanfinsmoura@gmail.com>
2023-12-31 16:56:21 +0000

    changed icons

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,11 +2,11 @@ import { proxy } from 'valtio';
 
 const state = proxy({
   intro: true,
-  color: '#EFBD48',
+  color: '#355E3B',
   isLogoTexture: true,
   isFullTexture: false,
-  logoDecal: './threejs.png',
-  fullDecal: './threejs.png',
+  logoDecal: './face.svg',
+  fullDecal: './pattern.svg',
 });
 
 export default state;
